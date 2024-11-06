a98404dffe941de72ff61197a4aac6c5f7e7d3bc
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-06 18:45:38 +0530

    All code add 9th time commit

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,52 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// const publicRoutes = ["/", "/api/webhooks", "/sign-in", "/sign-up"];
+
+// export default clerkMiddleware({
+//   publicRoutes,
+//   async afterAuth(auth, req) {
+//     if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+//       return NextResponse.redirect(new URL("/sign-in", req.url));
+//     }
+//     if (auth.userId && publicRoutes.includes(req.nextUrl.pathname)) {
+//       return NextResponse.redirect(new URL("/", req.url));
+//     }
+//   },
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+import { authMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const publicRoutes = ["/api/webhooks", "/sign-in", "/sign-up"];
+
+export default authMiddleware({
+  publicRoutes,
+  async afterAuth(auth, req) {
+    if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    if (auth.userId && publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    // Exclude Next.js internals and static files by pattern
+    "/((?!_next/static|_next/image|favicon.ico|public|.*\\.(?:css|js|json|jpg|jpeg|png|gif|svg|ico|woff|woff2|ttf|webp)).*)",
+    // Include API and trpc routes
+    "/api/(.*)",
+    "/trpc/(.*)",
   ],
 };
